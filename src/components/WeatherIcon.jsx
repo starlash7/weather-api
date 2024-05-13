e9ee575c5c1ef4e7f3ab7b1893435fc56a0177c3
starlash7 <yang92sw@gmail.com>
2024-05-13 16:35:53 +0900

    Geolocation, Axios

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -1,14 +1,38 @@
+import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
 
 const WeatherIcon = () => {
   const [latitude, setLatitude] = useState();
-  const [longitude, setLongtude] = useState();
+  const [longitude, setLongitude] = useState();
 
-  navigator.geolocation.getCurrentPosition((position) => {
-    setLatitude(position.coords.latitude);
-    setLongitude(position.coords.longitude);
-  });
+  const getWeather = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${
+          import.meta.env.VITE_API_KEY
+        }&units=metric`
+      );
+
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!latitude) return;
+
+    getWeather();
+  }, [latitude]);
+
   return <div>WeatherIcon</div>;
 };
 
